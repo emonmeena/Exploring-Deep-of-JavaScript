f6bdb1de78bf2fc4d128c35b437c26f7698663cf
mayank-genesis <mayank16meena@gmail.com>
2020-05-21 02:30:27 +0530

    ->JavaScript Advanced

---
+++ JS Advanced/JSAdvanced.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class JSAdvanced extends Component{
+    render(){
+        return(
+            <div>
+                foo bar
+            </div>
+        );
+    }
+}
--- js-libraries-base/src/App.js
+++ js-libraries-base/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import JSAdvanced from './JS Advanced/JSAdvanced'
 
 function App() {
   return (
     <div className="App">
-     
-     
+    <JSAdvanced/>
     </div>
   );
 }
