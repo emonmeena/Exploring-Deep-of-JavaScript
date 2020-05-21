406d68badc9e9ab93f1b16e888996258aa11172e
mayank-genesis <mayank16meena@gmail.com>
2020-05-21 10:38:33 +0530

    upto Functions

--- JS Advanced/JSAdvanced.js
+++ JS Advanced/JSAdvanced.js
@@ -1,10 +1,10 @@
 function paraTxtChange(){
-    return  document.getElementById("para").innerHTML= "Para changed"    //this is simple function or non Arrow function
+    return  document.getElementById("para").innerHTML= "Para changed"    //this is simple function or non Arrow function.
 }
 
 paraChange=()=> { 
     paraTxtChange();
-    document.getElementById("para").style.fontSize= "24px"  //this is Arrow function
+    document.getElementById("para").style.fontSize= "24px"  //this is Arrow function.
     document.getElementById("paraChanger").innerHTML="Done"
 }
 
@@ -22,11 +22,28 @@ bulbSwitch=()=> {
            
 }
 
-//In JS redeclaring the variable will not destroy the value stored
-//Array and Object declaration in JS
+//In JS redeclaring the variable will not destroy the value stored.
+//Array and Object declaration in JS.
 var arr = ["This", "is", "Array"];
 var object = {
     arr,
     i:10,
     string: "Beautiful"
 };
+
+/* 
+accessing objects property *using .property  or *using [property].
+but to access functions in an object we need either object.function() -method- or object.function -property.
+Accessing a function without () will always return function definition instead of it's result.
+endUptoFunctions */
+
+/* 
+Backslash before quotes accepts it as quotes
+string.length
+string.indexOf("text") shows first occurence of the "text"
+string.match(/text/g) g stands for - global, hence it searches for text globally and returns if found
+string.replace("text", "new text")
+toUpperCase() and toLowerCase()
+string.split("splitter") -imp- splits the string for splitter and returns an array
+endUptoSrings 
+*/
